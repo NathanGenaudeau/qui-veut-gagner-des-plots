e98f26e6c564ab6204b468bbc23a90b7ef1698c6
Nathan Genaudeau <nathan.genaudeau@gmail.com>
2022-11-16 22:44:09 +0100

    tests

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default {
-  base: '/qui-veut-gagner-des-plots/',
   plugins: [vue()],
   alias: {
     '@': path.resolve(__dirname, '/src'),
