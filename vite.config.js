4e9306a5be7cb0a5f7d5b61b5640030ab7dd5661
Nathan Genaudeau <71818668+NathanGenaudeau@users.noreply.github.com>
2022-11-16 22:08:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default {
+  base: '/qui-veut-gagner-des-plots/',
   plugins: [vue()],
   alias: {
     '@': path.resolve(__dirname, '/src'),
